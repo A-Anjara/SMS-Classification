7eb0159a916b5bdc730b088a37426dc3a5796cf6
A-Anjara <andriamihajamananaanjara@gmail.com>
2026-05-19 10:27:01 +0300

    Backend

--- backend/mongodb-backend/server.js
+++ backend/mongodb-backend/server.js
@@ -5,7 +5,8 @@ const { MongoClient } = require("mongodb");
 
 const app = express();
 const PORT = process.env.PORT;
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_ATLAS_URI;
+const MODEL_BACKEND_URI = process.env.MODEL_BACKEND_URI;
 app.use(cors());
 app.use(express.json());
 
@@ -16,7 +17,7 @@ async function connectDB() {
     const client = new MongoClient(MONGODB_URI);
     await client.connect();
     console.log("Connected Successfully to MongoDB");
-    db = client.db();
+    db = client.db('safeinbox');
   } catch (error) {
     console.log("Error connecting DB");
     process.exit(1);
@@ -42,7 +43,9 @@ app.post("/api/signin", async (req, res) => {
 app.post("/api/login", async (req, res) => {
   try {
     const { email, password } = req.body;
-    let result = await db.collection("user").findOne({ 'email': email, password: password });
+    let result = await db
+      .collection("user")
+      .findOne({ email: email, password: password });
     if (!result) {
       res.json({
         error: "fail",
@@ -59,46 +62,51 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.post("/api/list", async (req,res)=>{
-    const { email } = req.body;
-    try{
-        
-        let resultat = await db.collection('historique').find({'email':email}).toArray();
-        
-        
-        res.json({
-            message:"success",
-            reponse:resultat
-        })
-    }catch (error){
-        
-        res.json({
-            error: "fail"
-        })
-    }
+app.post("/api/list", async (req, res) => {
+  const { email } = req.body;
+  try {
+    let resultat = await db
+      .collection("historique")
+      .find({ email: email })
+      .toArray();
+
+    res.json({
+      message: "success",
+      reponse: resultat,
+    });
+  } catch (error) {
+    res.json({
+      error: "fail",
+    });
+  }
 });
 
 app.post("/api/verifier", async (req, res) => {
   try {
     const { email, message } = req.body;
-    
+
     if (message.length > 0) {
-        let resultat = "spam"; // // // // HARDCODE
+      let resultat = await fetch(`${MODEL_BACKEND_URI}/predict`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: "your sms text here" }),
+      });
+      resultat = await resultat.json();
       if (email.length > 0) {
-        let result = await db.collection("historique").insertOne({ 'email': email, 'message': message, 'resultat': resultat });
+        let result = await db
+          .collection("historique")
+          .insertOne({ email: email, message: message, resultat: resultat.answer });
       }
       res.json({
         message: "success",
-        reponse: resultat,
+        reponse: resultat.answer,
       });
-    }
-    else{
-
-        res.json(
-            {
-                error:"echec"
-            }
-        )
+    } else {
+      res.json({
+        error: "echec",
+      });
     }
   } catch (error) {
     res.json({
